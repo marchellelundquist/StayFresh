338f1c16483bf7596819aa082d1126b8022ed201
Marchelle Lundquist <marchellelundquist@gmail.com>
2015-11-14 17:38:13 -0500

    first commit

--- StayFresh/ViewController.swift
+++ StayFresh/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  StayFresh
+//  HHWhatever
 //
 //  Created by Marchelle Lundquist on 11/14/15.
 //  Copyright © 2015 HackHarvard. All rights reserved.
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textFieldUsername: UITextField!
+    
+    
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func verifyMethod(sender: AnyObject)
+    {
+        let usr = "Kevin"
+        let pw = "Sparky15"
+        
+        if textFieldUsername.text == usr && textFieldPassword.text == pw
+        {
+            theLabel.text = "Credentials correct"
+            theLabel.textColor=UIColor.greenColor()
+            textFieldUsername.resignFirstResponder()
+            textFieldPassword.resignFirstResponder()
+            performSegueWithIdentifier("goToFridge", sender: self)
+        }
+        else
+        {
+            theLabel.text = "Credentials incorrect"
+            theLabel.textColor=UIColor.redColor()
+            textFieldUsername.resignFirstResponder()
+            textFieldPassword.resignFirstResponder()
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
